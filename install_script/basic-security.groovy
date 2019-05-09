b89c431ed392ae4bb11b69fd9bbdccedd6409c8a
saidimohamed <saidimohamedali@hotmail.fr>
2019-05-09 11:20:34 +0200

    Update basic-security.groovy

--- install_script/basic-security.groovy
+++ install_script/basic-security.groovy
@@ -3,7 +3,13 @@
 import jenkins.model.*
 import hudson.util.*;
 import jenkins.install.*;
+import hudson.security.*
 
 def instance = Jenkins.getInstance()
 
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin","jenkins")
+instance.setSecurityRealm(hudsonRealm)
+instance.save()
