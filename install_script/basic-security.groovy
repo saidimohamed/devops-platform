616456f72b6082284570a6f2d7ee0b5987976807
saidimohamed <saidimohamedali@hotmail.fr>
2019-05-10 12:25:47 +0200

    Update basic-security.groovy

--- install_script/basic-security.groovy
+++ install_script/basic-security.groovy
@@ -9,6 +9,10 @@ def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount("admin","jenkins")
 instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+
 instance.save()
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 
