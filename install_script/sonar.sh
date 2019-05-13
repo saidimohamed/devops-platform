b21ef25dd2744621d1645a704d3ee9157f1dee10
saidimohamed <saidimohamedali@hotmail.fr>
2019-05-13 14:13:40 +0200

    Update sonar.sh

--- install_script/sonar.sh
+++ install_script/sonar.sh
@@ -2,7 +2,7 @@
 
 sudo yum -y install epel-release
 sudo yum -y update
-sudo yum install -y java-1.8.0-openjdk.x86_64 wget
+sudo yum install -y java-1.8.0-openjdk.x86_64 wget unzip
 
 
 sudo cp /etc/profile /etc/profile_backup
@@ -21,3 +21,31 @@ su - postgres
 createuser sonar
 psql -c "ALTER USER sonar WITH ENCRYPTED password 'sonar'"
 psql -c "CREATE DATABASE sonar OWNER sonar"
+
+https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.7.zip
+unzip sonarqube-7.7.zip -d /opt
+mv /opt/sonarqube-7.7 /opt/sonarqube
+
+sed -i -e 's/^#sonar.jdbc.username.*/sonar.jdbc.username="sonar"/g' /opt/sonarqube/conf/sonar.properties
+sed -i -e 's/^#sonar.jdbc.password.*/sonar.jdbc.password="sonar"/g' /opt/sonarqube/conf/sonar.properties
+sed -i -e 's/^#sonar.jdbc.url.*/sonar.jdbc.url=jdbc:postgresql:\/\/localhost\/sonar/g' /opt/sonarqube/conf/sonar.properties
+
+echo '[Unit]
+Description=SonarQube service
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
+ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+
+User=root
+Group=root
+Restart=always
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/sonar.service
+
+sudo systemctl start sonar
+sudo systemctl enable sonar
