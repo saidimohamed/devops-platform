86472daedf158c119870b869ec84958ef1bda15f
saidimohamed <saidimohamedali@hotmail.fr>
2019-05-13 14:49:02 +0200

    Update sonar.sh

--- install_script/sonar.sh
+++ install_script/sonar.sh
@@ -22,30 +22,27 @@ su - -c  "createuser sonar" postgres
 su - -c "psql -c \"ALTER USER sonar WITH ENCRYPTED password 'sonar'\"" postgres
 su - -c  "psql -c \"CREATE DATABASE sonar OWNER sonar\"" postgres
 
-https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.7.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.7.zip
 unzip sonarqube-7.7.zip -d /opt
 mv /opt/sonarqube-7.7 /opt/sonarqube
 
 sed -i -e 's/^#sonar.jdbc.username.*/sonar.jdbc.username="sonar"/g' /opt/sonarqube/conf/sonar.properties
 sed -i -e 's/^#sonar.jdbc.password.*/sonar.jdbc.password="sonar"/g' /opt/sonarqube/conf/sonar.properties
-sed -i -e 's/^#sonar.jdbc.url=jdbc:postgresql://localhost/sonar.*/sonar.jdbc.url=jdbc:postgresql:\/\/localhost\/sonar/g' /opt/sonarqube/conf/sonar.properties
+sed -i -e 's/^#sonar.jdbc.url=jdbc:postgresql:\/\/localhost\/sonar.*/sonar.jdbc.url=jdbc:postgresql:\/\/localhost\/sonar/g' /opt/sonarqube/conf/sonar.properties
 
 echo '[Unit]
 Description=SonarQube service
 After=syslog.target network.target
-
 [Service]
 Type=forking
-
 ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-
 User=root
 Group=root
 Restart=always
-
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/sonar.service
 
 sudo systemctl start sonar
 sudo systemctl enable sonar
+
