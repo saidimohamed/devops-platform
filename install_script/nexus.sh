0b4c3f573537985f91ccdfbe7963bbace34f3a5f
saidimohamed <saidimohamedali@hotmail.fr>
2020-03-04 17:26:47 +0100

    Update nexus.sh

--- install_script/nexus.sh
+++ install_script/nexus.sh
@@ -5,4 +5,4 @@
 #docker volume create --name nexus-data
 mkdir /usr/nexus-data
 chmod 200 R /usr/nexus-data
-docker run -d -p 8081:8081 --name nexus -v nexus-data:/nexus-data sonatype/nexus3
+docker run -d -p 8081:8081 --name nexus -v /usr/nexus-data:/nexus-data sonatype/nexus3
