18feefcce451c64e48e900cd3f2ed827daae5069
saidimohamed <saidimohamedali@hotmail.fr>
2020-03-04 12:38:25 +0100

    Update sonar_nexus.sh

--- install_script/sonar_nexus.sh
+++ install_script/sonar_nexus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#docker run -d --name sonarqube -p 9000:9000 sonarqube:7.5-community
+docker run -d --name sonarqube -p 9000:9000 sonarqube:7.5-community
 
 docker volume create --name nexus-data
 docker run -d -p 8081:8081 --name nexus -v nexus-data:/nexus-data sonatype/nexus3
